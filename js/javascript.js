6f85b0451c32c8b4440ccca9b6f0b68559048bf5
Tatyana Vogel <tatyana.vogel@gibmit.ch>
2020-02-27 17:16:40 +0100

    Load the data from API

--- js/javascript.js
+++ js/javascript.js
@@ -1,42 +1,60 @@
 $(function () {
-    $.getJSON("http://sandbox.gibm.ch/berufe.php")
-        .done(function (data) {
-            $('#job').empty().append('<option value="">Select your job </option>');
-            $.each(data, function (indexInArray, job) { 
-                $('<option value="' + job.beruf_id + '">' + job.beruf_name + '</option>')
-                .appendTo($('#job'));
-            });
-        })
-        .fail(function() {
-            console.log("job Server Error");
-        });
-    $("#job").change(function () {
-        const choosenJob = $(this).children("option:selected").val();
-        $.getJSON("http://sandbox.gibm.ch/klassen.php", {beruf_id : choosenJob})
+    //showPreloader();
+    showData();
+
+    function showData(params) {
+        getJobs().then(function (jobs) { 
+            console.log(jobs)
+            const selectOptions = jobs.map((job) => '<option value=' + job.beruf_id + '>' + job.beruf_name + '</option>')
+            $('#jobSelector').append(selectOptions) // show the data
+        }); 
+
+        getShoolClasses().then(function (shoolClasses) {
+            const selectOptions = shoolClasses.map((shoolClass) => '<option value=' + shoolClass.klasse_id + '>' + shoolClass.klasse_name + '</option>')
+            $('#shoolClassSelector').append(selectOptions) // show the data
+        }); 
+    }
+
+    // if ($.status < 4){
+    //     $("body").after('<p>loading</p>')
+    //     console.log("loading");
+    //     exit;
+    // }
+
+    $("body")
+    .after('<select name="" id="shoolClassSelector"></select>')
+    .after('<select name="" id="jobSelector"></select>')
+    
+
+    function getJobs() { 
+        return $.getJSON("http://sandbox.gibm.ch/berufe.php", null)
             .done(function (data) {
-                $('#class').empty().append('<option value="">Select your class</option>');
-                $.each(data, function (indexInArray, classes) { 
-                    $('<option value="' + classes.klasse_id + '">' + classes.klasse_name + '</option>')
-                    .appendTo($('#class'));
-                });
+                return data
+            })
+            .fail(function() {
+                console.log("job Server Error");
+                return []
+            });
+        }
+
+    
+    function getShoolClasses(param) {  
+        return $.getJSON("http://sandbox.gibm.ch/klassen.php", param ? {beruf_id : param} : null)
+            .done(function (data) {
+                return data
             })
             .fail(function() {
                 console.log("classes Server Error");
-            })
+                return []
+            });
+    }
 
-            }
-        );
-      });
+    $('#jobs').change(function () {
+        const choosenJob = $(this).children("option:selected").val();
+        console.log(choosenJob);
+        getClasses(choosenJob);
+    });
+});
 
-    // xmlhttp = new XMLHttpRequest();
-    // xmlhttp.onreadystatechange = function () {
-    //     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    //         document.getElementById("text").innerHTML = xmlhttp.responseText;
-
-    //     } 
-    // xmlhttp.open("GET", "http://sandbox.gibm.ch/berufe.php", true);
-    // xmlhttp.send();
-        
-    // }
 //.post werde ich brauchen um vom einten feld di infos zu nehmen und nur die gefilterten infos an das naechste feld zu senden. 
 // CROS mit php nicht vergessen?
